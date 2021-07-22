4b35e405d09398125e6d9630307ca903edee1671
osefatul <sefaullahomar2@gmail.com>
2021-07-22 05:42:46 -0700

    useForm hook has been updted

--- src/SendMail.js
+++ src/SendMail.js
@@ -2,7 +2,15 @@ import "./SendMail.css";
 import React from "react";
 import CloseIcon from "@material-ui/icons/Close";
 import { Button } from "@material-ui/core";
+import { useForm } from "react-hook-form";
 function SendMail() {
+  //we are using a react hook to modify Form
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="sendMail">
       <div className="sendMail_header">
@@ -10,11 +18,17 @@ function SendMail() {
         <CloseIcon className="sendMail_close" />
       </div>
 
-      <form>
-        <input placeholder="To" type="text" />
-        <input placeholder="Subject" type="text" />
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          placeHolder="Message..."
+          {...register("test", { required: true })} //this is registering this element to the React form hook
+          name="to"
+          placeholder="To"
+          type="text"
+        />
+        <input name="subject" placeholder="Subject" type="text" />
+        <input
+          name="message"
+          placeholder="Message..."
           type="text"
           className="sendMail_message"
         />
