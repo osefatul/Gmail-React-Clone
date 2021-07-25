16943bb7f5a242b3ec0f4b0fad9748b38b14aaa8
osefatul <sefaullahomar2@gmail.com>
2021-07-25 11:42:01 -0700

    Login page has been rendered successfully

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
@@ -6,30 +6,51 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Mail from "./Mail";
 import EmailList from "./EmailList";
 import SendMail from "./SendMail";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        //the user is loggedIn
+        dispatch(login);
+      } else {
+      }
+    });
+  }, []);
+
   return (
     <Router>
-      <div className="App">
-        {/*Header and Sidebar should be outside the switch, means whenever we click on the tab header and sidebar shouldn;t go away*/}
-        <Header />
-        <div className="app_body">
-          <Sidebar />
+      {/*If there is no user, render the Login page */}
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="App">
+          {/*Header and Sidebar should be outside the switch, means whenever we click on the tab header and sidebar shouldn;t go away*/}
+          <Header />
+          <div className="app_body">
+            <Sidebar />
 
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          {sendMessageIsOpen && <SendMail />}
         </div>
-        {sendMessageIsOpen && <SendMail />}
-      </div>
+      )}
     </Router>
   );
 }
