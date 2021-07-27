6b57309a95b0351cb705b048596bc39b1464b01b
osefatul <sefaullahomar2@gmail.com>
2021-07-26 18:28:22 -0700

    EmailList.js has been updated

--- src/EmailList.js
+++ src/EmailList.js
@@ -7,10 +7,6 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
 import SettingsIcon from "@material-ui/icons/Settings";
-import InboxIcon from "@material-ui/icons/Inbox";
-import PeopleIcon from "@material-ui/icons/People";
-import LocalOfferIcon from "@material-ui/icons/LocalOffer";
-import EmailRow from "./EmailRow";
 
 import "./EmailList.css";
 import Section from "./Section";
@@ -18,26 +14,6 @@ import { db } from "./firebase";
 
 function EmailList() {
   const [emails, setEmails] = useState([]);
-
-  // const tabs = [
-  //   { id: 0, category: "Primay", data: emails, Icon: InboxIcon, color: "red" },
-  //   {
-  //     id: 1,
-  //     category: "Social",
-
-  //     Icon: PeopleIcon,
-  //     color: "#1A73E8",
-  //   },
-  //   {
-  //     id: 2,
-  //     category: "Promotion",
-
-  //     Icon: LocalOfferIcon,
-  //     color: "green",
-  //   },
-  // ];
-  // const [value, setValue] = useState(0);
-  // const { id, category, data } = tabs[value];
 
   useEffect(() => {
     //snapshot is a realtime listener, if the database of emails is being modified or something has been added to it, then get that and run it again and then put it back on setEmails variable. docs are the each record of submitting email form [to,subjec,messafe]
@@ -87,62 +63,8 @@ function EmailList() {
       </div>
 
       <div className="emailList_sections">
-        {/* {tabs.map((item, index) => {
-          return (
-            <div
-              key={item.id}
-              onClick={() => setValue(index)}
-              className={`section ${index === value && "section-selected"}`}
-              style={{
-                borderBottom: `3px solid ${item.color}`,
-                color: `${item.color}`,
-              }}
-            >
-              <item.Icon />
-              <h4>{item.category}</h4>
-            </div>
-          );
-        })} */}
-
         <Section emails={emails} />
-
-        {/*
-        <Section Icon={InboxIcon} title="Primary" color="red" selected />
-        <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
-        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
-
-     
-        {active.objects.map((element, index) => {
-          return (
-            <Section
-              key={element}
-              Icon={element.icon}
-              title={element.title}
-              color={element.color}
-              onClick={() => toggleActive(index)}
-              className={toggleActiveStyles(index)}
-            />
-          );
-        })}
-
-       */}
       </div>
-
-      {/* <div className="emailList_list">
-        {data &&
-          data.map(({ id, data: { to, subject, message, timestamp } }) => {
-            return (
-              <EmailRow
-                id={id}
-                key={id}
-                title={to}
-                subject={subject}
-                description={message}
-                time={new Date(timestamp?.seconds * 1000).toUTCString()}
-              />
-            );
-          })}
-      </div> */}
     </div>
   );
 }
